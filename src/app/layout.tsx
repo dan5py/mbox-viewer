88d0b580a891c317da34e3b67514c7ad9a3e3e27
dan5py <git@dan5py.com>
2026-03-21 13:33:37 +0100

    fix: remove viewport maximumScale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export const viewport: Viewport = {
   themeColor: "#0f172a",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
 };
 
 export async function generateMetadata(): Promise<Metadata> {
